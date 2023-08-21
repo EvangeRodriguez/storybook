f185406110dbc834a4e58196f5100e847ee08e03
EvangeRodriguez <eva.irodriguez93@gmail.com>
2023-08-22 01:35:56 +0200

    storybook

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -6,14 +6,16 @@ import "./Button.css"
 
 const getStyles = (...args) => ["button", ...args].filter(Boolean).join(" ");
 
+//Constante Boton que recibe dos parametros children (contenido interno) y type(primario, secundario, terciario) 
 const Button = ({ children, type })=> { 
     return <button className ={getStyles(type)}> {children} 
     </button>
 }
 
+//Propiedades de los botones 
 Button.propTypes = {
-    children: PropTypes.string.isRequired,
-    type: PropTypes.oneOf(["primary", "secondary", "tertiary", ])
+    children: PropTypes.string.isRequired, //espera una cadena y es obligatoria 
+    type: PropTypes.oneOf(["primary", "secondary", "tertiary", ]) //espera recibir el tipo de cada uno 
 }
 
 export default Button; 
